cad4dd6e7362d971ce6691c1b80cb9ab5a451c4b
Alanna <phan.alanna@gmail.com>
2023-10-19 13:20:16 -0400

    add 13 more query files

---
+++ 4_queries/13_name_of_teachers_and_number_of_assistances.sql
@@ -0,0 +1,8 @@
+SELECT teachers.name AS teacher, cohorts.name AS cohort, count(assistance_requests) AS total_assistances
+FROM teachers
+JOIN assistance_requests ON teachers.id = teacher_id
+JOIN students ON students.id = student_id
+JOIN cohorts ON cohorts.id = cohort_id
+GROUP BY teacher, cohort
+HAVING cohorts.name = 'JUL02'
+ORDER BY teacher;
